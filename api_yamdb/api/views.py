2102f1da7bd90bc98414b2df728b6cdfc953ba2b
Александр Ш <method4354@yandex.ru>
2023-04-30 02:02:11 +0500

    deploy

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -143,12 +143,12 @@ def send_confirmation_code(username, email):
     user = get_object_or_404(User, email=email, username=username)
     confirmation_code = default_token_generator.make_token(user)
     user.confirmation_code = confirmation_code
-    MESSAGE = (
+    message = (
         f'Здравствуйте, {username}!'
         f'Ваш код подтверждения: {user.confirmation_code}'
     )
     send_mail(
-        message=MESSAGE,
+        message=message,
         subject='Confirmation code',
         recipient_list=[user.email],
         from_email=settings.DEFAULT_FROM_EMAIL,
