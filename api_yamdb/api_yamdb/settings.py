f954b656fea6b53411fa1b2b2b392dbaaacc8e8b
methodologyCode <113935966+methodologyCode@users.noreply.github.com>
2023-05-02 17:44:13 +0500

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.107.62']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
